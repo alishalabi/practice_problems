6d243c9167842d9c424869dac8c76f3567a23a9d
Ali Shalabi <ashalabi13@gmail.com>
2021-01-04 15:55:41 -0800

    Simple solution

--- gradeschool.py
+++ gradeschool.py
@@ -16,7 +16,7 @@ Get a sorted list of all students in all grades. Grades should sort as 1, 2, 3, 
 Note that all our students only have one name. (It's a small town, what do you want?)
 """
 
-student1 = ("Alice", 1)
+student1 = ("Zennny", 1)
 student2 = ("Bobby", 2)
 student3 = ("Cathy", 1)
 student4 = ("Dennis", 5)
@@ -34,3 +34,24 @@ class School:
         }
 
     def add_student(self, student):
+        self.roster[student[1]].append(student[0])
+        return self.roster
+
+    def get_grade(self, grade):
+        print(f"All students in grade {grade}: {self.roster[grade]}")
+        return self.roster[grade]
+
+    def all_students(self):
+        for index in range(len(self.roster.keys()) - 1):
+            grade = index + 1
+            sorted_students = sorted(self.roster[grade])
+            print(f"Students in grade {grade}: {sorted_students}")
+
+
+test_school = School()
+test_school.add_student(student1)
+test_school.add_student(student2)
+test_school.add_student(student3)
+test_school.add_student(student4)
+# test_school.get_grade(1)
+test_school.all_students()
