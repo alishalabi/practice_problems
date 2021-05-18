4bbf04fe35294d9b14aaf8cdffd9ca5f3acfc51a
Ali Shalabi <ashalabi13@gmail.com>
2021-05-18 12:26:25 -0500

    Working on trie for prefix tree method 2

--- longest_prefix.py
+++ longest_prefix.py
@@ -10,6 +10,8 @@ strs = ["dog", "racecar", "car"]
 """
 
 sample1 = ["flowers", "flow", "flight"]
+
+# Method 1: brute force, nested loops
 def longest_prefix(array):
     ret = ""
     shortest = min(sample1, key=len)
@@ -19,6 +21,35 @@ def longest_prefix(array):
                 return ret
         ret += shortest[index]
     return ret
+
+# Method 2: prefix tree
+class Node:
+    def __init__(self, data):
+        self.data = None
+        self.children = {}
+
+class PrefixTree:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current.children:
+                current.children[letter] = Node()
+            current.children[letter] = current
+
+    def build(self, words):
+        for word in words:
+            self.insert(word)
+
+    def longest_prefix(self):
+        ret = ""
+        current = self.root
+        if len(current.children.keys()) == 1:
+            ret += current.children.keys()[0]
+            current.
+
 
 
 print(longest_prefix(sample1))
