9f080377184db56ec47631b77b45181f9752f144
Ali Shalabi <ashalabi13@gmail.com>
2021-08-09 12:30:33 -0700

    Finished sum of multiples

---
+++ sum_of_multiples.py
@@ -0,0 +1,29 @@
+"""
+Given a number, find the sum of all the unique multiples of particular numbers
+up to but not including that number.
+
+If we list all the natural numbers below 20 that are multiples of 3 or 5, we get
+3, 5, 6, 9, 10, 12, 15, and 18.
+
+The sum of these multiples is 78.
+
+You can make the following assumptions about the inputs to the sum_of_multiples
+function:
+
+All input numbers are non-negative ints, i.e. natural numbers including zero.
+A list of factors must be given, and its elements are unique and sorted in
+ascending order.
+"""
+
+target = 20
+multiples = [3, 5]
+
+def sum_of_multiples(target, multiples):
+    output = []
+    for multiple in multiples:
+        for number in range(target):
+            if number % multiple == 0 and number not in output:
+                output.append(number)
+    return sum(output)
+
+print(sum_of_multiples(target, multiples))
