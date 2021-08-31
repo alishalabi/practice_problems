b248542e6e92cd760ebe315b0e0dbc011df6423d
Ali Shalabi <ashalabi13@gmail.com>
2021-08-30 18:48:12 -0700

    starting how_many_toppings

---
+++ how_many_toppings.py
@@ -0,0 +1,11 @@
+"""
+A burger restaurant wants to be the first to offer 1,000,000 possible combinations of
+toppings.
+How many menu items will they need?
+"""
+
+import combinations
+
+# input = ["ketchup", "lettuce", "mustard", "pickles", "onions", "cheese"]
+#
+# print(combinations.all_combinations(input))
