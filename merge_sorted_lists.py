749c40b7a297e2fe0d28b6456be25b2000704818
Ali Shalabi <ashalabi13@gmail.com>
2021-09-09 17:23:30 -0700

    Refactored merge sorted list

--- merge_sorted_lists.py
+++ merge_sorted_lists.py
@@ -33,10 +33,9 @@ def merge_lists(LL1, LL2):
     ret = LinkedList()
     first_node = LL1.head
     second_node = LL2.head
-    while first_node.next != None:
-        if second_node.next == None:
-            ret.add_node(first_node)
-            first_node = first_node.next
+
+    # Refactor
+    while first_node != None and second_node != None:
         if first_node.data < second_node.data:
             ret.add_node(first_node)
             first_node = first_node.next
@@ -44,16 +43,41 @@ def merge_lists(LL1, LL2):
             ret.add_node(second_node)
             second_node = second_node.next
 
-    while second_node.next != None:
-        if first_node.next == None:
-            ret.add_node(second_node)
-            second_node = second_node.next
-        if second_node.data < first_node.data:
-            ret.add_node(second_node)
-            second_node = second_node.next
-        else:
-            ret.add_node(first_node)
-            first_node = first_node.next
+    while first_node != None:
+        ret.add_node(first_node)
+        first_node = first_node.next
+
+    while second_node != None:
+        ret.add_node(second_node)
+        second_node = second_node.next
+
+    # Pre Refactor
+    # while first_node.next != None:
+    #     if second_node.next == None:
+    #         ret.add_node(first_node)
+    #         first_node = first_node.next
+    #     if first_node.data < second_node.data:
+    #         ret.add_node(first_node)
+    #         first_node = first_node.next
+    #     else:
+    #         ret.add_node(second_node)
+    #         second_node = second_node.next
+    #
+    # while second_node.next != None:
+    #     if first_node.next == None:
+    #         ret.add_node(second_node)
+    #         second_node = second_node.next
+    #     if second_node.data < first_node.data:
+    #         ret.add_node(second_node)
+    #         second_node = second_node.next
+    #     else:
+    #         ret.add_node(first_node)
+    #         first_node = first_node.next
+    #
+    # if first_node != None:
+    #     ret.add_node(first_node)
+    # if second_node != None:
+    #     ret.add_node(second_node)
 
     return ret
 
@@ -62,12 +86,18 @@ LL1 = LinkedList()
 LL1.add_node(Node(1))
 LL1.add_node(Node(2))
 LL1.add_node(Node(4))
+LL1.add_node(Node(7))
+LL1.add_node(Node(9))
 # LL1.print_LL()
 
 LL2 = LinkedList()
 LL2.add_node(Node(1))
 LL2.add_node(Node(3))
 LL2.add_node(Node(4))
+LL1.add_node(Node(8))
+LL1.add_node(Node(20))
+LL1.add_node(Node(21))
+LL1.add_node(Node(22))
 # LL2.print_LL()
 
 test = merge_lists(LL1, LL2)
